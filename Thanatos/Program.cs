da0894f1207d99599913513e11f1a0758a3fbd28
Archie <archiegeorgecunningham@gmail.com>
2026-02-26 22:49:06 +0000

    Start of the project.

--- Thanatos/Program.cs
+++ Thanatos/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using Discord;
+using Discord.WebSocket;
+using DotNetEnv;
+
+class Program
+{
+    private DiscordSocketClient _client;
+
+    static async Task Main(string[] args)
+        => await new Program().RunBotAsync();
+
+    public async Task RunBotAsync()
+    {
+        Env.Load();
+        string token = Environment.GetEnvironmentVariable("DISCORD_TOKEN")!;
+
+        _client = new DiscordSocketClient();
+        _client.Log += LogAsync;
+        _client.MessageReceived += MessageReceivedAsync;
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+
+    private Task LogAsync(LogMessage log)
+    {
+        Console.WriteLine(log);
+        return Task.CompletedTask;
+    }
+
+    private async Task MessageReceivedAsync(SocketMessage message)
+    {
+        if (message.Content == "!ping")
+            await message.Channel.SendMessageAsync("Pong!");
+    }
+}
